e37fd283c52de154da8b055956a32e28a782ff36
anshrv <zxctokyoghoul10007@gmail.com>
2023-10-20 00:03:00 +0500

    Калькулятор с новыми функÐциями

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "calculate2"
+include(":app")
+ 
